75993744955574b52710c76c148a0dc9539484e3
Baibhab Bhadra <baibhabbhadra5454@gmail.com>
2022-01-27 01:57:52 +0530

    Added main Class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+                Bank bank=new Bank("Type Bank name");
+                bank.addBranch("Add branch name");
+                bank.addCustomer("Add customer name","Add branch name",0);// taking initial amount
+                bank.addCustomerTransaction("Add customer name","Add branch name",0);//taking transaction
+                bank.listCustomers("Add branch name",true);// write false for not printing transactions.
 
     }
+
 }
